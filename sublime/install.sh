ce21c05526835d94dbbfca21e69669f5ecfee78a
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2015-07-27 23:56:40 +0400

    Restructured Sublime's preferences.

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env zsh
 printf "${Green}Installing Sublime's preferences.${Red}\n"
-ln -s ".dotfiles/sublime" "$HOME/.config/sublime-text-3/Packages/User"
+echo "${Yellow}Do you wish to remove Sublime preferences folder?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) rm -rf "$HOME/.config/sublime-text-3/Packages/User"; break;;
+        No ) exit;;
+    esac
+done
+ln -s "$HOME/.dotfiles/sublime" "$HOME/.config/sublime-text-3/Packages/User"
 
