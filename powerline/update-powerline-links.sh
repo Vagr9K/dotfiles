2d8ca36ea1adf0bd0a068b86b08f7b000ccca986
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-02-18 00:32:21 +0400

    Replaces old tmux powerline with new Powerline powered one.

--- powerline/update-powerline-links.sh
+++ powerline/update-powerline-links.sh
@@ -3,10 +3,12 @@
 siteroot=$(python -c "import site; print(site.getsitepackages()[0])")
 pkgroot="$siteroot/powerline"
 zshbind="$pkgroot/bindings/zsh/powerline.zsh"
+tmuxbind="$pkgroot/bindings/tmux/powerline.conf"
 unitname="powerline-daemon.service"
 
 echo "Updating Powerline symlinks."
 ln -sf "$zshbind" "$HOME/.dotfiles/powerline/powerline.zsh"
+ln -sf "$tmuxbind" "$HOME/.dotfiles/powerline/powerline.conf"
 
 echo "Enabling systemd service."
 systemctl --user enable "$unitname"
