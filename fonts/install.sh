f74cea295f73f002cbffc343e83093bc32b90fcc
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-05-05 00:42:36 +0400

    Updates fonts install script.

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 # printf "${Green}Installing Powerline fonts.${Red}\n"
 # ./fonts/powerline.sh
-printf "${Green}Installing Source Code Pro fonts.${Red}\n"
-./fonts/sourcecodepro.sh
-
+#printf "${Green}Installing Source Code Pro fonts.${Red}\n"
+#./fonts/sourcecodepro.sh
+printf "${Green}Installing Nerd Fonts.${Red}\n"
+pacaur -S nerdfont-complete
