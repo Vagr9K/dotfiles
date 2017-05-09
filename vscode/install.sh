f347937fc0966ef103521a26c0fecb1705b8c793
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-05-10 00:41:46 +0400

    Adds support for VSCode Insiders build.

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env zsh
 printf "${Green}Installing VSCode's preferences.${Red}\n"
+
+# Install for VSCode
 BasePath="$HOME/.config/Code/User"
+
+SettingsLink="$BasePath/settings.json"
+SettingsPath="$HOME/.dotfiles/vscode/settings.json"
+printf "${Green}Symlinking settings.json.${Red}\n"
+rm -i "$SettingsLink"
+ln -s "$SettingsPath" "$SettingsLink"
+
+SnippetsLink="$BasePath/snippets"
+SnippetsPath="$HOME/.dotfiles/vscode/snippets"
+printf "${Green}Symlinking snippets.${Red}\n"
+rm -ir "$SnippetsLink"
+ln -s "$SnippetsPath" "$SnippetsLink"
+
+KeybindingsLink="$BasePath/keybindings.json"
+KeybindingsPath="$HOME/.dotfiles/vscode/keybindings.json"
+printf "${Green}Symlinking settings.json.${Red}\n"
+rm -i "$KeybindingsLink"
+ln -s "$KeybindingsPath" "$KeybindingsLink"
+
+# Install for VSCode Insiders
+BasePath="$HOME/.config/Code - Insiders/User"
 
 SettingsLink="$BasePath/settings.json"
 SettingsPath="$HOME/.dotfiles/vscode/settings.json"
