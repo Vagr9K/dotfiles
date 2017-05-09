1746f172bcf34f23c9619f2c5ea0da8c565ce4ac
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-05-10 01:39:59 +0400

    Fixes history-substring-search not working.

--- zsh/config/history-substring-search.zsh
+++ zsh/config/history-substring-search.zsh
@@ -4,5 +4,5 @@ HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
 HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
 
 # Key Bindings
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+bindkey '\eOA'   history-substring-search-up
+bindkey '\eOB' history-substring-search-down
