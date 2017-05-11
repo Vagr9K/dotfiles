63d9a304942f8c050b3c5082ad6b15c165e54f0b
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-05-11 13:58:11 +0400

    Adds history file adjustments.

--- zsh/config/history.zsh
+++ zsh/config/history.zsh
@@ -1,21 +1,20 @@
 # History file path.
 HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"
 # Internal history max size.
-HISTSIZE=10000
+HISTSIZE=2000
 # History file max size
-SAVEHIST=5000
+SAVEHIST=2000
 
-setopt BANG_HIST
-setopt EXTENDED_HISTORY
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_FIND_NO_DUPS
-setopt HIST_SAVE_NO_DUPS
-setopt HIST_VERIFY
-setopt HIST_BEEP
-
-# Ignore commands starting with space
-setopt histignorespace
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing nonexistent history.
