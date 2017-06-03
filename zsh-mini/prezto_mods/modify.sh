52cf0fdd1937a6904f9da255af834aa7c6c515d6
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-06-04 00:10:04 +0400

    Removed GRML theme.

--- zsh-mini/prezto_mods/modify.sh
+++ zsh-mini/prezto_mods/modify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-MyTheme="$HOME/.dotfiles/zsh-mini/prezto_mods/grml/grml"
+MyTheme="$HOME/.dotfiles/zsh-mini/prezto_mods/agnoster2/agnoster2"
 echo "Adding new prompt."
-ln -s "$MyTheme" "$HOME/.dotfiles/zsh-mini/.zprezto/modules/prompt/functions/prompt_grml_setup"
+ln -s "$MyTheme" "$HOME/.dotfiles/zsh-mini/.zprezto/modules/prompt/functions/prompt_agnoster2_setup"
